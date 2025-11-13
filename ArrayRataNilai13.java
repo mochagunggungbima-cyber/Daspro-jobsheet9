11ec7cc63e0ec6e38c04bffb21bd87b16b0b1c01
Moch.Agung Bima Ardian <bimaagungbimzz@gmail.com>
2025-11-13 20:22:48 +0700

    Modifikasi Percobaan 3 Pertanyaan 2

--- ArrayRataNilai13.java
+++ ArrayRataNilai13.java
@@ -3,24 +3,38 @@ import java.util.Scanner;
 public class ArrayRataNilai13 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int[] nilaiMhs = new int[10];
-        double total = 0;
-        double rata2;
+
+        System.out.print("Masukkan jumlah mahasiswa: ");
+        int jumlahMhs = sc.nextInt();
+        int[] nilaiMhs = new int[jumlahMhs];
+
+        double totalLulus = 0, totalTidakLulus = 0;
+        int jumlahLulus = 0, jumlahTidakLulus = 0;
+
         for (int i = 0; i < nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+" : ");
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
             nilaiMhs[i] = sc.nextInt();
         }
+
+        System.out.println("\n=== Hasil Penilaian ===");
         for (int i = 0; i < nilaiMhs.length; i++) {
             if (nilaiMhs[i] > 70) {
-                System.out.println("Nilai mahasiswa ke-"+(i+1)+" : "+nilaiMhs[i]+" = lulus!");
+                System.out.println("Mahasiswa ke-" + (i + 1) + " : " + nilaiMhs[i] + " = LULUS");
+                totalLulus += nilaiMhs[i];
+                jumlahLulus++;
             } else {
-            System.out.println("Nilai mahasiswa ke-"+(i+1)+" : "+nilaiMhs[i]+" = tidak lulus!");
+                System.out.println("Mahasiswa ke-" + (i + 1) + " : " + nilaiMhs[i] + " = TIDAK LULUS");
+                totalTidakLulus += nilaiMhs[i];
+                jumlahTidakLulus++;
             }
         }
-        for (int i = 0; i < nilaiMhs.length; i++) {
-            total += nilaiMhs[i];
-        }
-        rata2 = total/nilaiMhs.length;
-        System.out.println("Rata-rata nilai = "+rata2);
+
+        double rataLulus = jumlahLulus > 0 ? totalLulus / jumlahLulus : 0;
+        double rataTidakLulus = jumlahTidakLulus > 0 ? totalTidakLulus / jumlahTidakLulus : 0;
+
+        System.out.println("\nRata-rata nilai LULUS       : " + rataLulus);
+        System.out.println("Rata-rata nilai TIDAK LULUS : " + rataTidakLulus);
+
+        sc.close();
     }
 }
